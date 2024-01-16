63c4a27d4e030e1e2230dffbf57a89a04c5cc6d7
JeromeChantelauze <156183115+JeromeChantelauze@users.noreply.github.com>
2024-01-16 11:46:25 +0200

    Updated by upload

--- replace.sh
+++ replace.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-awk 'NR >= 63 && NR <= 69 { origin = $6; print origin, " was repaced  by ith the letter: [A, U, T, O, M, A, T, I, O, N]" }' log.txt > change.log
+Count=63
+AWK_S1=""
+AWK=$AWK"NR < 63 { print \$0 }\n"
 
-gawk -i inplace 'NR < 63 { print $0 }
-                 NR >= 63 && NR <= 69 { $6 = "AUTOMATION"; print $0 }
-                 NR > 69 { print $0 }' log.txt
+rm -f change.log && touch change,log
+
+for Name in A U T O M A T I O N; do
+	AWK=$AWK"NR ==$Count { origin = \$6;  \$6 = \"$Name\"; print origin, \" was repaced  by ith the letter $Name\" >> \"change.log\" ; print \$0 }\n"
+	Count=$(( Count + 1 ))
+done
+
+AWK=$AWK"NR >= $Count { print \$0 }\n"
+
+echo -e $AWK > /tmp/$$.awk
+gawk -i inplace -f /tmp/$$.awk log.txt
+#rm /tmp/$$.awk
 
 exit 0
